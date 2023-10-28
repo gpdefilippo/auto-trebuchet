645a7781569aa34834315ca05d9d86ebe756546b
gpdefilippo <gpdefilippo@gmail.com>
2023-10-28 18:48:18 -0500

    getting selenium started

--- trebuchet.py
+++ trebuchet.py
@@ -1,1 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+firefox_options = Options()
+# firefox_options.headless = True
+
+driver = webdriver.Firefox(options=firefox_options)
+driver.get('https://virtualtrebuchet.com/')
+
+button = driver.find_element_by_class_name("svelte-1pi9qli")
+button.click()
+driver.implicitly_wait(60)
+
+driver.quit()
