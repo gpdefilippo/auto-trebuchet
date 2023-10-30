304e8b3d01ea2d66dc5c1efcb9f864afc9c36795
gpdefilippo <gpdefilippo@gmail.com>
2023-10-30 17:22:30 -0500

    initial finding of elements in page

--- trebuchet.py
+++ trebuchet.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
 
 firefox_options = Options()
 # firefox_options.headless = True
@@ -7,8 +10,31 @@ firefox_options = Options()
 driver = webdriver.Firefox(options=firefox_options)
 driver.get('https://virtualtrebuchet.com/')
 
-button = driver.find_element_by_class_name("svelte-1pi9qli")
+speed = driver.find_element_by_id("playSpeed")
+for _ in range(5):
+    speed.send_keys(Keys.ARROW_RIGHT)
+
+short_arm = driver.find_element_by_id("lengthArmShort")
+short_arm.clear()
+short_arm.send_keys("4", Keys.RETURN)
+
+mass_weight = driver.find_element_by_id("massWeight")
+mass_weight.clear()
+mass_weight.send_keys("70", Keys.RETURN)
+
+release_angle = driver.find_element_by_id("releaseAngle")
+release_angle.clear()
+release_angle.send_keys("70", Keys.RETURN)
+
+
+button = driver.find_elements_by_tag_name('button')[0]
 button.click()
-driver.implicitly_wait(60)
+
+distance = driver.find_element_by_xpath(
+        "/html/body/div/div[1]/main/div[2]/div/div/div[1]/div[1]/table/tbody/tr[1]/td[2]").text
+height = driver.find_element_by_xpath(
+        "/html/body/div/div[1]/main/div[2]/div/div/div[1]/div[1]/table/tbody/tr[2]/td[2]").text
+time = driver.find_element_by_xpath(
+    "/html/body/div/div[1]/main/div[2]/div/div/div[1]/div[1]/table/tbody/tr[3]/td[2]").text
 
 driver.quit()
