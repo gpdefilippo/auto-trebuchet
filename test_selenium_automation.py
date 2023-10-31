f221e4481a25a0889635502fbcb047da09609b5c
gpdefilippo <gpdefilippo@gmail.com>
2023-10-31 17:28:22 -0500

    adding jupyter notebook with run logic

--- test_selenium_automation.py
+++ test_selenium_automation.py
@@ -1,13 +1,26 @@
 import unittest
-from selenium.webdriver.common.keys import Keys
 
 from selenium_automation import SelTrebuchet
 
 
 class TestSelTrebuchet(unittest.TestCase):
-    def test_simulate_with_params(self):
+    def test_simulate_firefox(self):
         # Create a SelTrebuchet instance for testing
         with SelTrebuchet(browser='firefox') as trebuchet:
+            len_shortarm = 1.0
+            mass_weight = 50.0
+            angle_release = 60.0
+
+            distance, height, time = trebuchet.simulate(
+                len_shortarm, mass_weight, angle_release)
+
+        self.assertAlmostEqual(distance, 44.546, places=2)
+        self.assertAlmostEqual(height, 34.870, places=2)
+        self.assertAlmostEqual(time, 4.033, places=2)
+
+    def test_simulate_chrome(self):
+        # Create a SelTrebuchet instance for testing
+        with SelTrebuchet(browser='chrome') as trebuchet:
             len_shortarm = 1.0
             mass_weight = 50.0
             angle_release = 60.0
